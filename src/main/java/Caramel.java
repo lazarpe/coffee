7072ca1a29604a2064015171b4b8aa5bb39617bc
Davide Marcoli <davide@marcoli.ch>
2022-03-01 10:14:43 +0100

    Removed Package toppings

--- src/main/java/Caramel.java
+++ src/main/java/Caramel.java
@@ -1,5 +1,3 @@
-package toppings;
-
 public class Caramel {
     public static final String NAME = "Caramel";
     public static final double PRICE = 0.50;
--- src/main/java/WhipCream.java
+++ src/main/java/WhipCream.java
@@ -1,6 +1,4 @@
-package toppings;
-
-public class WhipCream {
+public class WhipCream extends CondimentDecorator {
     public static final String NAME = "Whip Cream";
     public static final double PRICE = 0.45;
 }
